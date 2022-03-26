73c0c3905df4e10b146df15fa515b5865b317156
That_Guy977 <sirawit.andy977@gmail.com>
2022-03-26 15:08:53 +0700

    Add finals OpMode

---
+++ Kappa_CKKRobotJunior.java
@@ -0,0 +1,6 @@
+package org.firstinspires.ftc.transfinity;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+@Autonomous(name="Autonomous (Finals w CKK Robot Junior) BC", preselectTeleOp="TeleOp (Blue)")
+public class Kappa_CKKRobotJunior extends KappaBlueCarousel {}
